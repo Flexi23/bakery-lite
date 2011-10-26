4abb0d942baf7cbe8053519a8f79c79f2f1ca6da
Felix Woitzel <Flexi23@Felixs-MacBook-Pro.local>
2011-10-26 23:26:33 +0200

    initial commit
    Soundcloud Canvas doodle.

---
+++ bakery-lite/js/raf.js
@@ -0,0 +1,8 @@
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame
+			|| window.mozRequestAnimationFrame || window.oRequestAnimationFrame
+			|| window.msRequestAnimationFrame
+			|| function(/* function */callback, /* DOMElement */element) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
